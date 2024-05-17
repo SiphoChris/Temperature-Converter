a46775448abf2d872b0748db7346c16083004df3
SiphoChris <siphonkebe@gmail.com>
2024-05-17 15:07:44 +0200

    html structure

---
+++ script/code.js
@@ -0,0 +1,5 @@
+let screen = document.querySelector("#screen");
+let inputValue = parseFloat(document.querySelector("#inputValue"));
+let unit = document.querySelector("#measure-of-unit");
+let btn = document.querySelector("#calc-btn");
+
