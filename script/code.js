cbf2723c727a1bac32cdea4b704edd8dbc379831
SiphoChris <siphonkebe@gmail.com>
2024-05-20 08:32:38 +0200

    ship it

--- script/code.js
+++ script/code.js
@@ -1,5 +1,18 @@
 let screen = document.querySelector("#screen");
-let inputValue = parseFloat(document.querySelector("#inputValue"));
-let unit = document.querySelector("#measure-of-unit");
+let inputValue = document.querySelector("#inputValue");
+let unit = document.querySelector("#unit-of-measure");
 let btn = document.querySelector("#calc-btn");
 
+btn.addEventListener("click", () => {
+  unit = unit.options[unit.selectedIndex].text;
+
+  if (unit == "To Farenheit") {
+    let farenValue = (+inputValue.value) * (1.8 + 32);
+    screen.value = farenValue;
+  } else if (unit == "To Celcius") {
+    let celiusValue = (+inputValue.value - 32) * (5 / 9);
+    screen.value = celiusValue;
+  } else {
+    throw new Error("user input not valid");
+  }
+});
